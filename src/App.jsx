00806b61defb98be0aa908ae519109eb1c18f891
Daniel Okobia <duokobia@gmail.com>
2023-05-11 10:25:06 +0100

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to Fietsa Hub</h2>
+      <p className="paragraph">Hi There! It is project time.</p>
+    </div>
+  );
+}
+
+export default App;
